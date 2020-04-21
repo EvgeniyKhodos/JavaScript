a509cf665084f9783528787fa7cb2455d1bbc5e1
EvgeniyKhodos <en-hodos@mail.ru>
2020-04-21 10:54:03 +0700

    First commit

---
+++ lesson 1/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarrid: false
+};
+
+console.log(persone.name);
+
+
+// alert("Hello world");
+
+// let answer = confirm("Are you here?");
+
+console.log("Evgeny" + " Khodos");
