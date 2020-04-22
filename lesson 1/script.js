b545750411864c7e6dad6835a8c91fd6bbba54f2
EvgeniyKhodos <en-hodos@mail.ru>
2020-04-22 17:01:34 +0700

    1 Lesson

--- lesson 1/script.js
+++ lesson 1/script.js
@@ -1,18 +1,20 @@
 "use strict";
 
-let persone = {
-    name: 'John',
-    age: 25,
-    isMarrid: false
+let answer = prompt("Введите обязательную статью расходов в этом месяце", "Еда");
+
+let money = prompt("Во сколько обойдется?", "1000");
+
+let appData = {
+    money: 2000,
+    timeData: 0,
+    expenses: {
+        answer: money
+    },
+    optionalExpenses: 0,
+    income: 0,
+    savings: false
 };
 
-console.log(persone.name);
+console.log(appData.expenses.answer);
 
-
-// alert("Hello world");
-
-// let answer = confirm("Are you here?");
-
-console.log("Evgeny" + " Khodos");
-
-alert("Test");
+alert("Расходы на день: " + appData.expenses.answer + (appData.expenses.answer / 30));
